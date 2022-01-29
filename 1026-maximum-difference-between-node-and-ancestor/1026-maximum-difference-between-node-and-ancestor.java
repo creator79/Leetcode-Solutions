3ac3aeab8c4fd1c1fedbaf7a85ae5ec28565e496
Vivek <72148636+mahi-19@users.noreply.github.com>
2022-01-29 15:46:49 +0530

    Time: 0 ms (100.00%), Space: 42.8 MB (5.79%) - LeetHub

--- 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
+++ 1026-maximum-difference-between-node-and-ancestor/1026-maximum-difference-between-node-and-ancestor.java
@@ -13,25 +13,39 @@
  *     }
  * }
  */
-   class Solution {
+//    class Solution {
 
-    public int maxAncestorDiff(TreeNode root) {
-        return this.traverse(root, root.val, root.val); // trick to avoid diff for root element
-    }
+//     public int maxAncestorDiff(TreeNode root) {
+//         return this.traverse(root, root.val, root.val); // trick to avoid diff for root element
+//     }
 
-    private int traverse(TreeNode root, int max, int min) {
-        if (root == null) return max - min; // leaf, return result
+//     private int traverse(TreeNode root, int max, int min) {
+//         if (root == null) return max - min; // leaf, return result
 
-        //  calculate new min and max for current tree path including current element
-        var newMax = Math.max(max, root.val);
-        var newMin = Math.min(min, root.val);
+//         //  calculate new min and max for current tree path including current element
+//         var newMax = Math.max(max, root.val);
+//         var newMin = Math.min(min, root.val);
 
-        // visit children
-        var current = newMax - newMin;
-        var left = this.traverse(root.left, newMax, newMin);
-        var right = this.traverse(root.right, newMax, newMin);
+//         // visit children
+//         var current = newMax - newMin;
+//         var left = this.traverse(root.left, newMax, newMin);
+//         var right = this.traverse(root.right, newMax, newMin);
 
-        // return best result
-        return Math.max(current, Math.max(left, right));
-    }
+//         // return best result
+//         return Math.max(current, Math.max(left, right));
+//     }
+// }
+
+
+
+class Solution {
+public int maxAncestorDiff(TreeNode root) {
+return md(root,root.val,root.val);
+}
+public int md(TreeNode root, int mn, int mx) {
+if (root == null) return mx - mn;
+mx = Math.max(mx, root.val);
+mn = Math.min(mn, root.val);
+return Math.max(md(root.left, mn, mx), md(root.right, mn, mx));
+}
 }
