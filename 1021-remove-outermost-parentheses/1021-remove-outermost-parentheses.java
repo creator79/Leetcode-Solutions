a95aed3c4af55a4d9b64335f240af84432ed8e86
Vivek <72148636+mahi-19@users.noreply.github.com>
2022-02-10 20:27:30 +0530

    Time: 10 ms (35.04%), Space: 42.9 MB (24.72%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -5,6 +5,10 @@ class Solution {
       for(int i=0 ; i<s.length();i++){
         char ch = s.charAt(i);
         if(ch=='('){
+//st.size()!=0 This lies is check of 
+// logic 1 stack agar empty hogya toh vo Outermost Parentheses last pop element hoga
+// logic 2 firstly opening brackets ke liye closing brackets check karo agar stack empty hai toh ans mat bnaao agara stack empty nhai hai toh usse string builder me add karke ans bnaa lo 
+// yahi humara main logic hai ki hume inner parentheses ko apna ans bana hai 
           if(st.size()!=0){
             sb.append('(');
           }
