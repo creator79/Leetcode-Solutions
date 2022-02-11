80293b75b6adc9144996fe2e2f5878def6036ba4
Vivek <72148636+mahi-19@users.noreply.github.com>
2022-02-11 19:27:49 +0530

    Time: 35 ms (38.88%), Space: 52.6 MB (15.09%) - LeetHub

---
+++ 1249-minimum-remove-to-make-valid-parentheses/1249-minimum-remove-to-make-valid-parentheses.java
@@ -0,0 +1,35 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+ char arr[]=s.toCharArray();
+        int count=0;
+        Stack<Integer> st=new Stack<>();
+        for(int i=0;i<arr.length;i++){
+            char ch=arr[i];
+            if(ch=='('){
+                st.push(i);
+            }else if(ch==')'){
+                if(st.size()==0){
+                    arr[i]='@';
+                    count++;
+                }else{
+                    st.pop();
+                }
+            }
+        }
+        while(st.size()>0){
+            arr[st.pop()]='@';
+            count++;
+        }
+        
+        char ans[]=new char[arr.length-count];
+        int idx=0;
+        
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]!='@'){
+                ans[idx]=arr[i];
+                idx++;
+            }
+        }
+        return new String(ans);
+    }
+}
