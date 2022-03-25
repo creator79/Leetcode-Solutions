2fa8bdcb62674d10d6d9aa935d116b8597b42078
Vivek <72148636+creator79@users.noreply.github.com>
2022-03-26 00:24:11 +0530

    Time: 3 ms (52.68%), Space: 41.9 MB (62.56%) - LeetHub

--- 20-valid-parentheses/20-valid-parentheses.java
+++ 20-valid-parentheses/20-valid-parentheses.java
@@ -1,23 +1,42 @@
+// class Solution {
+//     public boolean isValid(String s) {
+//         Stack<Character> st=new Stack<>();
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             char ch=s.charAt(i);
+//             if(st.isEmpty() && (ch==']' || ch=='}' || ch==')'))
+//                 return false;
+//             if(ch=='(' || ch=='{' || ch=='[')
+//                 st.push(ch);
+//             else{
+//                 if(ch==')' && st.pop()!='(')
+//                     return false;
+//                 if(ch==']' && st.pop()!='[')
+//                     return false;
+//                 if(ch=='}' && st.pop()!='{')
+//                     return false;
+//             }
+//         }
+//         return st.isEmpty();
+//     }
+// }
 class Solution {
     public boolean isValid(String s) {
-        Stack<Character> st=new Stack<>();
+        Stack<Character> stack = new Stack<>();
         
-        for(int i=0;i<s.length();i++)
-        {
-            char ch=s.charAt(i);
-            if(st.isEmpty() && (ch==']' || ch=='}' || ch==')'))
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(')');
+            } else if (s.charAt(i) == '{') {
+                stack.push('}');
+            } else if (s.charAt(i) == '[') {
+                stack.push(']');
+            } else if (stack.isEmpty() || stack.pop() != s.charAt(i)) {
                 return false;
-            if(ch=='(' || ch=='{' || ch=='[')
-                st.push(ch);
-            else{
-                if(ch==')' && st.pop()!='(')
-                    return false;
-                if(ch==']' && st.pop()!='[')
-                    return false;
-                if(ch=='}' && st.pop()!='{')
-                    return false;
             }
         }
-        return st.isEmpty();
+        
+        return stack.isEmpty();
     }
 }
