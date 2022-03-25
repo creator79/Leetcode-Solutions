b89643a0f74f129ee4645c9d5b1c2375ef0beba4
Vivek <72148636+creator79@users.noreply.github.com>
2022-03-25 23:25:30 +0530

    Added solution - LeetHub

---
+++ Find the Safe Position - GFG/find-the-safe-position.cpp
@@ -0,0 +1,36 @@
+// { Driver Code Starts
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+
+class Solution {
+  public:
+    int safePos(int n, int k) {
+        // code here 
+        if (n == 1)
+        return 1;
+    else
+        /* The position returned by josephus(n - 1, k)
+        is adjusted because the recursive call
+        josephus(n - 1, k) considers the
+        original position k % n + 1 as position 1 */
+        return (safePos(n - 1, k) + k - 1) % n + 1;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n,k;
+        
+        cin>>n>>k;
+
+        Solution ob;
+        cout << ob.safePos(n,k) << endl;
+    }
+    return 0;
+}  // } Driver Code Ends
