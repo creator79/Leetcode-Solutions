76a18a0092144487c6812f2b308ad0db6d765715
Vivek <72148636+creator79@users.noreply.github.com>
2022-04-11 16:59:28 +0530

    Time: 5 ms (33.33%), Space: 41.3 MB (33.33%) - LeetHub

--- 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
+++ 2231-largest-number-after-digit-swaps-by-parity/2231-largest-number-after-digit-swaps-by-parity.java
@@ -1,30 +1,33 @@
 class Solution {
     public int largestInteger(int num) {
-      PriorityQueue<Integer> oddPQ = new PriorityQueue<>((a,b) ->(b-a));
-       PriorityQueue<Integer> evenPQ = new PriorityQueue<>((a,b) ->(b-a));
+      PriorityQueue<Integer> evenPQ = new PriorityQueue<>((a,b) -> (b-a));
+      PriorityQueue<Integer> oddPQ = new PriorityQueue<>((a,b) -> (b-a));
       
-      String str = num+"";
-          // iterate through Array
-      for(char c: str.toCharArray()){
-         // extract each digit
+      String str = num + "";
+      for(char c:str.toCharArray()){
+        // extract each digit 
         int el = c-'0';
-        // check its even or not
-        if(el%2==0){
+        // check it is even or odd
+        if (el%2==0){
           evenPQ.offer(el);
-        }else{
+        }
+        else{
           oddPQ.offer(el);
         }
       }
         String ans = "";
-      // iterate through input Array that is num
-      for(char c: str.toCharArray()){
+      for (char c: str.toCharArray()){
         int el = c-'0';
         if(el%2==0){
-          ans+= (evenPQ.poll()+"");
-        } else{
-          ans+= (oddPQ.poll()+"");
+           ans+= (evenPQ.poll()+"");
+        }
+        else{
+           ans+= (oddPQ.poll()+"");
         }
       }
-      return Integer.parseInt(ans);
+      return  Integer.parseInt(ans);
     }
 }
+
+
+
