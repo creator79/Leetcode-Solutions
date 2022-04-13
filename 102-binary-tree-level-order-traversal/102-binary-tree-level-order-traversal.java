ffb4feb3102a9dbe0a55582d8c8c075a1a478f75
Vivek <72148636+creator79@users.noreply.github.com>
2022-04-14 00:39:47 +0530

    Time: 0 ms (100.00%), Space: 42.5 MB (82.63%) - LeetHub

--- 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
+++ 102-binary-tree-level-order-traversal/102-binary-tree-level-order-traversal.java
@@ -14,33 +14,29 @@
  * }
  */
 class Solution {
-      public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> arr = new ArrayList();
-        Queue<TreeNode> q = new LinkedList<>();
+    public List<List<Integer>> levelOrder(TreeNode root) {
+      List<List<Integer>> ans = new ArrayList();
+      if(root==null) return ans;
+       Queue<TreeNode> q = new LinkedList<>();
+      q.add(root);
+      while(true){
+        int size = q.size();
+        if(size==0) return ans;
+        ArrayList<Integer> data = new ArrayList<>();
         
-        if(root==null)
-            return arr;
-        q.add(root);
-        
-        while(q.size()!=0)
-            
-        {
-            List<Integer> a = new ArrayList<>();
-           
-           int s = q.size();
+        while(size>0){
+          TreeNode temp = q.remove();
+          
+          data.add(temp.val);
+          if(temp.left!=null)
+            q.add(temp.left);
+          if(temp.right!=null)
+            q.add(temp.right);
+          size--;
+        }//while
+        ans.add(data);
+      }//while
+     // return ans;
       
-           for(int i=0;i<s;i++)
-           {
-               TreeNode curr = q.poll();
-               if(curr.left!=null)
-                    q.add(curr.left);
-                if(curr.right!=null)
-                    q.add(curr.right); 
-               a.add(curr.val);
-           }
-            arr.add(a);
-        }
-        
-      return arr;
     }
 }
