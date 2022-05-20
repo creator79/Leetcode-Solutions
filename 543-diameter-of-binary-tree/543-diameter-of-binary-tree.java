184328e8cbfce1da19ded3f4c8f1217be3864385
Vivek <72148636+creator79@users.noreply.github.com>
2022-05-20 10:57:58 +0530

    Time: 1 ms (50.14%), Space: 43.4 MB (44.04%) - LeetHub

--- 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
+++ 543-diameter-of-binary-tree/543-diameter-of-binary-tree.java
@@ -14,103 +14,18 @@
  * }
  */
 class Solution {
+    int ans=0;
     public int diameterOfBinaryTree(TreeNode root) {
-   // yaha pe refrencing ki chiz nahi hota hai issliye diameter ko array type  banaya hai
-        int[] diameter = new int[1];
-        height (root,diameter);
-//       jab diameter varibale ko array type ka banaya hai toh return bhi array type me hoga. 
-        return diameter[0];
-    }
-  private int height (TreeNode node,int[] diameter){
-    if (node == null){
-      return 0;
-    }
-    int lh=height(node.left,diameter);
-    int rh=height(node.right,diameter);
-     // jab diameter varibale ko function me array type ka banaya hai toh return bhi array type me hoga. 
-    diameter[0] = Math.max(diameter[0],lh+rh);
-    return 1+Math.max(lh,rh);
-  }
-}
-/********************************************************************************************************
-class Solution {
-    
-    int diameter = 0;
-    
-    public int height(TreeNode root){
-        
-        // Algorithm: Result = Max value of (1 + left_subtree_height + right_subtree_height)
-        
-        // Step 1: Base case
-        if(root == null){ return 0; }
-        
-        // Step 2: Calculate height of left sub tree
-        int leftSubTreeHeight = height(root.left);
-        
-        // Step 3: Calculate height of right sub tree
-        int rightSubTreeHeight = height(root.right);
-        
-        // Step 4: Find & store max value of (1 + left_height + right_height)
-        diameter = Math.max(diameter, 1 + leftSubTreeHeight + rightSubTreeHeight);
-        
-        // Step 5: Return height of current node
-        return 1 + Math.max(leftSubTreeHeight, rightSubTreeHeight);
-        
+         int data = height(root);
+        return ans-1;
     }
     
-    public int diameterOfBinaryTree(TreeNode root) {
-        
-        // Step 0 : Calculate the height at each level & store the max value in 'diameter'
-        height(root);
-        
-        return diameter - 1;
-        
-    }
-
-/******************************************************************************************************
-
-
-class Solution {
-    
-    int max = 0;
-    
-    public int diameterOfBinaryTree(TreeNode root) {
-        
-        diameterCalculate(root);
-        
-        return max;
-        
-    }
-    
-    private int diameterCalculate(TreeNode root) {
-        if (root == null) {
-            return 0;
+    private int height (TreeNode root){
+        if (root==null) return 0;
+        int lh = height(root.left);
+        int rh = height(root.right);
+        ans = Math.max(ans,1+lh+rh);
+        return Math.max(lh,rh)+1;
             
-        } else {
-            
-            int left = diameterCalculate(root.left);
-            int right = diameterCalculate(root.right);
-            
-            max = Math.max(left + right, max);
-            
-            return Math.max(left, right) + 1;
-        }
     }
 }
-
-
-/********************************************************************************************************/
-
-
-
-
-
-
-
-
-
-
-
-
-
-
