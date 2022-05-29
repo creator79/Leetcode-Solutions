33ec677c72c73eaedd1106e22e9ba028a4aafaed
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-05-29 20:26:51 +0530

    Time: 0 ms (100.00%), Space: 39.2 MB (91.69%) - LeetHub

---
+++ 191-number-of-1-bits/191-number-of-1-bits.java
@@ -0,0 +1,16 @@
+public class Solution {
+    // use keringhan algo
+    // you need to treat n as an unsigned value
+    public int hammingWeight(int n) {
+        int c=0;
+while(n!=0)
+{
+n=n&(n-1);
+c++;
+}
+return c;
+
+
+        
+    }
+}
