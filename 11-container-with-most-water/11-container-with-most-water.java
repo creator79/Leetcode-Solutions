5b8f42216bc2f9a967e47944cbc641e1add64a52
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-07-12 20:39:00 +0530

    Time: 3 ms (97.19%), Space: 52.6 MB (92.24%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.java
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -1,20 +1,38 @@
+// To get the maximum water, we can calculate using 2 loops. At each iteration, we require a distance between 2 lines and the minimum of those lines. So we start with i at 0 and j=i+1. Then calculate max at ar[i] for every other j. We keep a global max and when both loops are done, the value of the max is the answer.
+
+
+// class Solution {
+//     public int maxArea(int[] height) {
+        
+//         int i,j,mx=Integer.MIN_VALUE;
+//         int n=height.length;
+//         for(i=0;i<n;i++){
+//             for(j=i+1;j<n;j++){
+//                 int water = (j-i)*Math.min(height[j],height[i]);
+//                     mx=Math.max(mx,water); 
+//             }
+//         }
+        
+//         return mx;       
+//     }
+// }
+
+
+
+
+
+
 class Solution {
-    public int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int max = 0;
-        while(left < right){
-            int w = right - left;
-            int h = Math.min(height[left], height[right]);
-            int area = h * w;
-            max = Math.max(max, area);
-            if(height[left] < height[right]) left++;
-            else if(height[left] > height[right]) right--;
-            else {
-                left++;
-                right--;
-            }
-        }
-        return max;
+    public int maxArea(int[] ar) {
+         int i=0,j=ar.length-1,mx=Integer.MIN_VALUE;
+        while(i<j){
+		int water= (j-i)*Math.min(ar[i],ar[j]);
+		mx=Math.max(mx,water);
+		if(ar[i]<ar[j])i++;
+		else j--;
+		
+	}
+        
+        return mx;       
     }
 }
