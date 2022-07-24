4596544d11c95340e95d5fe303108d26fa85864b
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-07-24 19:49:31 +0530

    Time: 16 ms (16.93%), Space: 57.5 MB (63.56%) - LeetHub

--- 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
+++ 240-search-a-2d-matrix-ii/240-search-a-2d-matrix-ii.java
@@ -1,12 +1,24 @@
+// If value is > then target, then move to left i.e. j--
+
+// If value is < then target, then move down i.e. i++
+
+// If value is == to target, return true
+
+// Else return false
+// Brute force is done previously
+
+// tc= O(row + column)
+// sc=o(1)
+
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
-    int r=matrix.length;
-    int c=matrix[0].length;
-        for(int i=0;i<r;i++){
-            for(int j=0;j<c;j++){
-                if(matrix[i][j]==target)
-                    return true;
-            }
+        int i = 0;
+        int j = matrix[0].length - 1;
+        
+        while(i >= 0 && i < matrix.length && j >= 0 && j < matrix[0].length){
+            if(matrix[i][j] == target) return true;
+            else if(matrix[i][j] > target) j--;
+            else if(matrix[i][j] < target) i++;
         }
         return false;
     }
