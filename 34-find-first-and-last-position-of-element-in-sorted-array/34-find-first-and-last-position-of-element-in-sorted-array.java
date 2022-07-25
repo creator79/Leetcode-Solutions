49c3a6618387b1a7e78834330f53be0519ad5b80
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-07-25 19:58:06 +0530

    Time: 8 ms (7.77%), Space: 48.3 MB (6.08%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,92 +1,28 @@
-// //Brute Force Approach (Linear Search) : 
-// class Solution {
-//     public int[] searchRange(int[] nums, int target) {
-
-//       //Create Array to show result of occurence positions
-//       //Initialize with -1, because if target is not found in an Array
-//         int[] position = {-1, -1};
-
-//        //Searching for First Occurrence, therefore search left to right
-//         for(int i=0; i<nums.length; i++) {
-//             if(nums[i] == target) {
-//                 position[0] = i;
-//                 break;         //because, here we only have to find first occurence 
-//                                   //and not second ocuurence, third occurence , ...
-//              }
-//         }
-
-//        //Searching for Last Occurrence, therefore search right to left
-//         for(int i=nums.length-1; i>=0; i--) {
-//             if(nums[i] == target) {
-//                 position[1] = i;
-//                 break;          //because, here we only have to find last occurence 
-//                                    //and not second last ocuurence, third last ...
-//              }
-//         }
-
-//         return position;
-//     }
-// }
-
-
-
-//Optimized Approach (Binary Search) :
 class Solution {
-    public int[] searchRange(int[] nums, int target) {
-        
-        //Create Array to show result of occurence positions
-        int[] ans = new int[2];
-        
-        ans[0] = firstOccur(nums, target);
-        ans[1] = lastOccur(nums, target);
-        
-        return ans;
-    }
-    
-    public int firstOccur(int[] nums, int target) {
-        int start = 0 , end=nums.length-1;
-        
-        //Initialize with -1, because if target is not found in an Array
-        int position=-1;
-        
-        while(start<=end) {
-            int mid = (start + end) / 2;
-            if(nums[mid] < target) {
-                start = mid + 1;
-            }
-            else if(nums[mid] > target) {
-                end = mid -1;
-            }
-            else {
-                position = mid;
-                
- //Above we found our target, but don't know that it is which occurence               
- //So, to find first occurence, also see elements which are at left of mid
-                end = mid -1;
-            }
+
+    public int[] searchRange(int[] nums, int x) {
+        ArrayList<Integer> arr1 = new ArrayList<Integer>();
+
+        for (int i : nums) arr1.add(i);
+
+        int first = arr1.indexOf(x);
+        Collections.reverse(arr1);
+        int last = arr1.size() - 1 - arr1.indexOf(x);
+
+        ArrayList<Integer> ans = new ArrayList<Integer>();
+        ans.add(first);
+        ans.add(last);
+
+
+        if (!arr1.contains(x)) {
+         return  new  int  []  { -1, -1 };
         }
-        return position;
-    }
-    
-     public int lastOccur(int[] nums, int target) {
-        int start = 0 , end=nums.length-1 , position=-1;
-        while(start<=end) {
-            int mid = (start + end) / 2;
-            if(nums[mid] < target) {
-                start = mid + 1;
-            }
-            else if(nums[mid] > target) {
-                end = mid -1;
-            }
-            else {
-                 position = mid;
-                
- //Above we found our target, but don't know that it is which occurence  
- //So, to find last occurence, also see elements which are at right of mid
-                 start = mid + 1;
-            }
+
+        /*ArrayList to Array Conversion */
+        int array[] = new int[ans.size()];
+        for (int j = 0; j < ans.size(); j++) {
+            array[j] = ans.get(j);
         }
-        return position;
+        return array;
     }
-    
 }
