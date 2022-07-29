dcfda2086cca441e0aa20013f304204fb00a42e1
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-07-29 11:44:15 +0530

    Time: 2 ms (66.83%), Space: 41.1 MB (59.31%) - LeetHub

--- 202-happy-number/202-happy-number.java
+++ 202-happy-number/202-happy-number.java
@@ -1,20 +1,23 @@
 class Solution {
     public boolean isHappy(int n) {
         
-//         apoorach of slow and fast pointer used in LL
+// apoorach of slow and fast pointer used in LL
         int slow=n;
         int fast=n;
         do{
+//    Move by slow pointer means square only one times          
             slow=findSquare(slow);
+//     Move by fast pointer  means square the number two times         
             fast=findSquare(findSquare(fast));
             
         }while(slow!=fast);
-            if(slow==1){
+            if(slow==1){ // while  numbers for which this process ends in 1 are happy.
                 return true;
             }
         
         return false;     
     }
+//     Find the square digit by digit
     private int findSquare(int num){
         int ans=0;
         while(num>0){
