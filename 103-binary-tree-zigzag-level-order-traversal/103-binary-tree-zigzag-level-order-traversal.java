d64c84ec1c8c449fe736fdcfeeb4e399ef7f4179
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-08-02 23:48:31 +0530

    Time: 2 ms (38.81%), Space: 43 MB (26.92%) - LeetHub

--- 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
+++ 103-binary-tree-zigzag-level-order-traversal/103-binary-tree-zigzag-level-order-traversal.java
@@ -1,69 +1,44 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-
 class Solution {
+
     public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-        if(root==null) return new ArrayList<>();
+         
+        List<List<Integer>> ans = new ArrayList<>();
+        Queue<TreeNode> q = new LinkedList<>();
+         if (root == null) return ans;
+
+         q.add(root);
+          boolean  lfr = true;
         
-        Stack<TreeNode> stack1 = new Stack<>();
-        Stack<TreeNode> stack2 = new Stack<>();
-        List<List<Integer>> result = new ArrayList<>();
-        stack1.add(root);
-        
-        result.add(new ArrayList<>());
-        result.get(0).add(root.val);
-        int level = 0;
-        while(!stack1.isEmpty()){
-            int size = stack1.size();
-            level = 1 - level; // new level type (odd / even)
-            List<Integer> ans = new ArrayList<>();
-            for(int i=0; i<size; i++){
-                TreeNode node = stack1.pop();
-                if(level%2 != 0){ // if newLevel is odd then we have to add right first
-                    addRight(node, stack2, ans);
-                    addLeft(node, stack2, ans);
-                }else{ // if newLevel is even then we have to add left first
-                    addLeft(node, stack2, ans);
-                    addRight(node, stack2, ans);
+        while(!q.isEmpty()){
+            
+            int size=q.size();
+            List <Integer> arr = new ArrayList<>(size);
+            
+            for (int i=0;i<size;i++){
+                TreeNode front=q.poll();
+//                 
+                if(front.left!=null){
+                    q.add(front.left);
                 }
+                 
+                if(front.right!=null){
+                    q.add(front.right);
+                }
+                // if flag is true (left -> right)
+            if (lfr == true) arr.add(front.val);
+            // else (right -> left)
+            else arr.add(0, front.val);
+                
             }
-            /*
-            if all nodes of current level has been checked and we found an non-empty answer from this level,
-            we can add the same to our result.
-            */ 
-            if(!ans.isEmpty()){
-                result.add(ans);
-                stack1 = stack2;
-                stack2 = new Stack<>();
-            }
+
+            // changing flag for each level
+            lfr=!lfr;
+            
+           ans.add(arr);
+            
+            
         }
-        return result;
-    }
-    
-    static void addLeft(TreeNode currNode, Stack<TreeNode> stack, List<Integer> res){
-        if(currNode.left!=null){
-            stack.add(currNode.left);
-            res.add(currNode.left.val);
-        }
-    }
-    
-    static void addRight(TreeNode currNode, Stack<TreeNode> stack, List<Integer> res){
-        if(currNode.right!=null){
-            stack.add(currNode.right);
-            res.add(currNode.right.val);
-        }
+        return ans;
+        
     }
 }
