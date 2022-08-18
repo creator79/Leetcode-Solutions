0183d6dcdac7b270e69ab9c20b54f4941c494a67
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-08-18 17:43:23 +0530

    Time: 5 ms (66.37%), Space: 45.6 MB (44.60%) - LeetHub

--- 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
+++ 2273-find-resultant-array-after-removing-anagrams/2273-find-resultant-array-after-removing-anagrams.java
@@ -1,42 +1,17 @@
-class Solution{
-public boolean checkAnagram(String s, String t)
-    {
-        HashMap<Character,Integer> hs = new HashMap<>(); 
-        HashMap<Character, Integer> h = new HashMap<>();
-        if(s.length() > t.length()) 
-        {
-             return false;
+class Solution {
+//     its is slightly better apporach than creating another function of checking anagram using hashmap.
+    public List<String> removeAnagrams(String[] words) {
+         List<String> ans = new ArrayList<>();
+        String temp = "";
+        for (String s: words){
+            char[] carr = s.toCharArray();
+            Arrays.sort(carr);
+            String curr = new String(carr);
+            if (!curr.equals(temp))
+                ans.add(s);
+            temp = curr;
         }
-        for(Character ch: s.toCharArray()) 
-        {
-				hs.put(ch, hs.getOrDefault(ch,0) + 1); 
-        }
-        
-        for(Character c: t.toCharArray()) 
-        {
-            h.put(c,h.getOrDefault(c,0) +1);
-        }
-        if(h.equals(hs) == true) 
-        {
-            return true;
-        }
-        return false;
-    }
-    
-    public List<String> removeAnagrams(String[] words) 
-    {
-        List<String> res = new ArrayList<>();
-        String prev = "";
+        return ans;
 
-        for(int i = 0;i<words.length;i++)
-        {
-            if(checkAnagram(words[i], prev) == false)
-            {
-                res.add(words[i]);
-                prev = words[i];
-            }
-        }
-        return res;
-         
     }
 }
