fbdfd4a9e6fdb44ec725c1ba4ad0a2b95da62df4
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-08-21 15:36:59 +0530

    Time: 1 ms (42.86%), Space: 42.4 MB (42.86%) - LeetHub

---
+++ 2383-minimum-hours-of-training-to-win-a-competition/2383-minimum-hours-of-training-to-win-a-competition.java
@@ -0,0 +1,27 @@
+class Solution {
+// its a good easy question
+    public int minNumberOfHours(int initialEnergy, int initialExperience, int[] energy, int[] experience) {
+        int n = energy.length;
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (initialEnergy <= energy[i]) {
+                int tempEnergy = energy[i] + 1 - initialEnergy;
+                ans += tempEnergy;
+                initialEnergy += tempEnergy;
+            }
+
+            if (initialExperience <= experience[i]) {
+                int tempExperience = experience[i] + 1 - initialExperience;
+                ans += tempExperience;
+                initialExperience += tempExperience;
+            }
+
+            initialEnergy -= energy[i];
+            initialExperience += experience[i];
+        }
+
+        return ans;
+    }
+}
+// TC: O(n), SC: O(1)
