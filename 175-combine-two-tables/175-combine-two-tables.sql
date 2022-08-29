c004c42991e765f04a3ae1388de1644b636e5889
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-08-30 00:49:53 +0530

    Time: 333 ms (91.07%), Space: 0B (100.00%) - LeetHub

---
+++ 175-combine-two-tables/175-combine-two-tables.sql
@@ -0,0 +1,13 @@
+# Write your MySQL query statement below
+
+# SELECT Person.firstName , Person.lastName , Address.city ,Address.state
+# FROM Person 
+# INNER JOIN Address  ON Person.personId=Address .personId ;
+
+
+
+
+
+SELECT Person.firstName , Person.lastName, Address.city ,Address.state
+FROM Person
+LEFT JOIN Address ON Person.personId=Address.personId 
