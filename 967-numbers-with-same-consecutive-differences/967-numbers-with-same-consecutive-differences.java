2d6606cb06427b8604bf897d1976d1a41801b40b
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-09-04 00:07:07 +0530

    Time: 2 ms (95.31%), Space: 43 MB (51.25%) - LeetHub

--- 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
+++ 967-numbers-with-same-consecutive-differences/967-numbers-with-same-consecutive-differences.java
@@ -1,30 +1,33 @@
 class Solution {
     
-    public int[] numsSameConsecDiff(int n, int k) {
-            if (n == 1) {
-                return new int[]{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-            }
-            List<Integer> res = new ArrayList<>();
-            for (int i = 1; i <= 9; i++) {
-                dfs(i, n - 1, k, res);
-            }
-            return res.stream().mapToInt(i -> i).toArray();
+    private void dfs (int num, int digit, int n, int k, List<Integer> numbers) {
         
-            
-        
-    }
-
-    private void dfs(int num, int n, int k, List<Integer> res) {
         if (n == 0) {
-            res.add(num);
+            numbers.add (num);
             return;
         }
-        int lastDigit = num % 10;
-        if (lastDigit - k >= 0) {
-            dfs(num * 10 + lastDigit - k, n - 1, k, res);
+        if (digit + k <= 9) {
+            dfs (num * 10 + (digit + k), digit + k, n - 1, k, numbers);
         }
-        if (lastDigit + k <= 9 && k != 0) {
-            dfs(num * 10 + lastDigit + k, n - 1, k, res);
+        if (k != 0 && digit - k >= 0) {
+            dfs (num * 10 + (digit - k), digit - k, n - 1, k, numbers);
         }
+    }
+    
+    public int[] numsSameConsecDiff(int n, int k) {
+        
+        List<Integer> numbers = new ArrayList<> ();
+        
+        for (int num = 1; num < 10; num++) {
+            dfs (num, num, n - 1, k, numbers);
+        }
+        
+        int[] answer = new int[numbers.size ()];
+        
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = numbers.get (i);
+        }
+        
+        return answer;
     }
 }
