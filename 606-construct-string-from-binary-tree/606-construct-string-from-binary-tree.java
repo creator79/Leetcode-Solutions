9fd4719b27420ffb14280d1ebb7bdb494d492ea4
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-09-07 11:47:44 +0530

    Time: 155 ms (5.03%), Space: 161.6 MB (5.12%) - LeetHub

--- 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
+++ 606-construct-string-from-binary-tree/606-construct-string-from-binary-tree.java
@@ -14,19 +14,31 @@
  * }
  */
 class Solution {
-    String ans="";
+    String ans = "";
+
     public String tree2str(TreeNode root) {
         pass(root);
         return ans;
     }
-    public void pass(TreeNode root){
-        if(root.left==null&&root.right==null){
-            ans+=root.val;
-            return;}
-        ans+=root.val;
-        if(root.left!=null){ans+="(";pass(root.left);ans+=")";}
-         if(root.left==null&&root.right!=null){ans+="()";}
-        if(root.right!=null){ans+="(";pass(root.right);ans+=")";}
-       
+
+    public void pass(TreeNode root) {
+        if (root.left == null && root.right == null) {
+            ans += root.val;
+            return;
+        }
+        ans += root.val;
+        if (root.left != null) {
+            ans += "(";
+            pass(root.left);
+            ans += ")";
+        }
+        if (root.left == null && root.right != null) {
+            ans += "()";
+        }
+        if (root.right != null) {
+            ans += "(";
+            pass(root.right);
+            ans += ")";
+        }
     }
 }
