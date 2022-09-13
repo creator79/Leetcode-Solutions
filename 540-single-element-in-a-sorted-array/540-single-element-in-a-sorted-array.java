c83722bf65ea60cf1082c364bdbc609ef65b6b1d
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-09-13 23:39:35 +0530

    Time: 0 ms (100.00%), Space: 60.2 MB (69.00%) - LeetHub

--- 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/540-single-element-in-a-sorted-array.java
@@ -1,32 +1,30 @@
 class Solution {
 
     public int singleNonDuplicate(int[] nums) {
-        // Binary Search
-        //       int n = nums.length;
-        //     int l = 0, r = n - 1;
-        //     while (l < r) {
-        //         int m = l + (r - l) / 2;
-        //         if (m % 2 == 1) m--;
-        //         if (nums[m] == nums[m + 1]) l = m + 2;
-        //         else r = m;
-        //     }
-        //     return nums[l];
-
+        // ~~~~~~~~~~~~~~~~~~~~~~~~~~ Binary Search ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+        int n = nums.length;
+        int l = 0, r = n - 1;
+        while (l < r) {
+            int m = l + (r - l) / 2;
+            if (m % 2 == 1) m--;
+            if (nums[m] == nums[m + 1]) l = m + 2; else r = m;
+        }
+        return nums[l];
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~Hashmap~~~~~~~~~~~~~~~~~~~
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (map.containsKey(nums[i])) {
-                map.put(nums[i], map.get(nums[i]) + 1);
-            } else {
-                map.put(nums[i], 1);
-            }
-        }
-        for (int i = 0; i < nums.length; i++) {
-            if (map.get(nums[i]) == 1) {
-                return nums[i];
-            }
-        }
-        return 0;
+        // HashMap<Integer, Integer> map = new HashMap<>();
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.containsKey(nums[i])) {
+        //         map.put(nums[i], map.get(nums[i]) + 1);
+        //     } else {
+        //         map.put(nums[i], 1);
+        //     }
+        // }
+        // for (int i = 0; i < nums.length; i++) {
+        //     if (map.get(nums[i]) == 1) {
+        //         return nums[i];
+        //     }
+        // }
+        // return 0;
 
         // ~~~~~~~~~~~~~~~~~~~~~~~~~~~HashSet~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
         // HashSet<Integer> set = new HashSet<>();
