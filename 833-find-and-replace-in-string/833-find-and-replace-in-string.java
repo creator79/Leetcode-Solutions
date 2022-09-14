178dbe6dce2e564a3f3964ea3c4492fb76646b5c
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-09-14 11:57:37 +0530

    Time: 15 ms (12.57%), Space: 43.7 MB (20.66%) - LeetHub

--- 833-find-and-replace-in-string/833-find-and-replace-in-string.java
+++ 833-find-and-replace-in-string/833-find-and-replace-in-string.java
@@ -1,29 +1,24 @@
 class Solution {
 
     public String findReplaceString(String s, int[] indices, String[] sources, String[] targets) {
-        HashMap<Integer, String> map = new HashMap<>();
+         TreeMap<Integer, String> map = new TreeMap<>();
         for (int i = 0; i < indices.length; i++) {
             map.put(indices[i], sources[i] + " " + targets[i]);
         }
 
-        // Sort
-        Arrays.sort(indices);
-
-        // Replace
+        // StringBuilder
         StringBuilder sb = new StringBuilder();
         int i = 0;
-        int j = 0;
         while (i < s.length()) {
-            if (j < indices.length && i == indices[j]) {
-                String[] strs = map.get(indices[j]).split(" ");
-                if (s.substring(i, i + strs[0].length()).equals(strs[0])) {
-                    sb.append(strs[1]);
-                    i += strs[0].length();
+            if (map.containsKey(i)) {
+                String[] str = map.get(i).split(" ");
+                if (s.substring(i, i + str[0].length()).equals(str[0])) {
+                    sb.append(str[1]);
+                    i += str[0].length();
                 } else {
                     sb.append(s.charAt(i));
                     i++;
                 }
-                j++;
             } else {
                 sb.append(s.charAt(i));
                 i++;
@@ -31,5 +26,6 @@ class Solution {
         }
 
         return sb.toString();
+
     }
 }
