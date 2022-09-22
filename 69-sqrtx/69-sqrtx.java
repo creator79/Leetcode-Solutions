a13b8275530e1655d5a8e7f84260b3829c107df0
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2022-09-22 16:07:13 +0530

    Time: 3 ms (41.88%), Space: 40.6 MB (83.93%) - LeetHub

--- 69-sqrtx/69-sqrtx.java
+++ 69-sqrtx/69-sqrtx.java
@@ -1,5 +1,17 @@
 class Solution {
+
     public int mySqrt(int x) {
-        return (int)Math.sqrt(x);
+        if (x == 0) return 0;
+        int start = 1; // just handle two base case 0 and 1
+        int end = x;
+        int ans = 0;
+        while (start <= end) { // we use simple binary search here
+            int mid = start + (end - start) / 2;
+            if (mid <= x / mid) {
+                start = mid + 1;
+                ans = mid;
+            } else end = mid - 1;
+        }
+        return ans;
     }
 }
