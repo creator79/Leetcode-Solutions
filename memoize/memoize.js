bcd026ab891a04e1bb753ef4ba32896a5a7a540d
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2023-04-17 13:17:48 +0530

    Time: 402 ms (31.06%) | Memory: 75.8 MB (72.98%) - LeetSync

---
+++ memoize/memoize.js
@@ -0,0 +1,27 @@
+/**
+ * @param {Function} fn
+ */
+
+function memoize(fn) {
+    let answer = {};
+    return function(...args) {
+        const argsString = args.join(".");
+        const cacheValue = answer[argsString]
+        if(cacheValue != undefined) {
+            return cacheValue;
+        }
+        return answer[argsString] = fn(...args);
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
