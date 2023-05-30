16b1d95b018a3f01241611659526f4736f43ee0f
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2023-05-30 14:17:22 +0530

    Time: 16 ms (81.61%) | Memory: 48.5 MB (92.63%) - LeetSync

---
+++ design-hashset/design-hashset.java
@@ -0,0 +1,56 @@
+class MyHashSet {
+
+   boolean[] arr;
+
+    public MyHashSet() {
+
+     arr = new boolean [1000001];
+
+    }
+
+    
+
+    public void add(int key) {
+
+      arr[key] = true;  
+
+    }
+
+    
+
+    public void remove(int key) {
+
+      arr[key] = false;  
+
+    }
+
+    
+
+    public boolean contains(int key) {
+
+      return arr[key];  
+
+    }
+
+}
+
+
+
+   // https://leetcode.com/problems/design-hashset/discuss/1970118/Two-easiest-way-to-solve-using-JAVA
+        
+
+    
+        
+
+    
+
+
+
+
+/**
+ * Your MyHashSet object will be instantiated and called as such:
+ * MyHashSet obj = new MyHashSet();
+ * obj.add(key);
+ * obj.remove(key);
+ * boolean param_3 = obj.contains(key);
+ */
