c31a57033008fd747ecb595bb4411e7f1ec2cad1
Vivek Upadhyay <72148636+creator79@users.noreply.github.com>
2024-02-25 00:23:52 +0530

    Time: 59 ms (74.51%) | Memory: 49.3 MB (41.72%) - LeetSync

---
+++ 2859-add-two-promises/add-two-promises.js
@@ -0,0 +1,26 @@
+/**
+ * @param {Promise} promise1
+ * @param {Promise} promise2
+ * @return {Promise}
+ */
+var addTwoPromises = async function(promise1, promise2) {
+    let ans1=0; ans2=0;
+  await promise1.then(value1 => {
+      return ans1 = value1
+})
+  await promise2.then(value2 => {
+    return ans2= value2
+  
+})
+
+.catch(err => {
+  console.log(err);
+});
+    return ans1+ans2;
+
+};
+
+/**
+ * addTwoPromises(Promise.resolve(2), Promise.resolve(2))
+ *   .then(console.log); // 4
+ */
